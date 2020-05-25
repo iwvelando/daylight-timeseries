73a591430e51e40db4cfac9e0c4c57d5fe58ffd0
Isaac Velando <isaac@greplinux.com>
2020-05-25 13:02:30 -0500

    Initial version

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/iwvelando/daylight-timeseries
+
+go 1.13
+
+require (
+	github.com/influxdata/influxdb1-client v0.0.0-20200515024757-02f0bf5dbca3
+	github.com/nathan-osman/go-sunrise v0.0.0-20171121204956-7c449e7c690b
+	go.uber.org/zap v1.15.0
+)
---
+++ main.go
@@ -0,0 +1,168 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	_ "github.com/influxdata/influxdb1-client"
+	influxdb "github.com/influxdata/influxdb1-client"
+	"github.com/nathan-osman/go-sunrise"
+	"go.uber.org/zap"
+	"net/url"
+	"os"
+	"time"
+)
+
+// Config represents a JSON-formatted config file
+type Configuration struct {
+	Latitude                float64       `json:"latitude"`
+	Longitude               float64       `json:"longitude"`
+	PollInterval            time.Duration `json:"poll-interval"`
+	InfluxDBURL             string        `json:"influxdb-url"`
+	InfluxDBPort            int           `json:"influxdb-port"`
+	InfluxDBMeasurement     string        `json:"influxdb-measurement"`
+	InfluxDBDatabase        string        `json:"influxdb-database"`
+	InfluxDBRetentionPolicy string        `json:"influxdb-retention-policy"`
+}
+
+// Load a config file and return the Config struct
+func LoadConfiguration(logger *zap.Logger, file string) Configuration {
+
+	var config Configuration
+	configFile, err := os.Open(file)
+	defer configFile.Close()
+	if err != nil {
+		logger.Warn(fmt.Sprintf("failed to open %s", file),
+			zap.String("op", "LoadConfiguration"),
+			zap.Error(err),
+		)
+		return config
+	}
+
+	jsonParser := json.NewDecoder(configFile)
+	jsonParser.Decode(&config)
+
+	return config
+}
+
+func main() {
+
+	// Initialize the structured logger
+	logger, err := zap.NewProduction()
+	if err != nil {
+		fmt.Println("{\"op\": \"main\", \"level\": \"fatal\", \"msg\": \"failed to initiate logger\"}")
+		os.Exit(1)
+	}
+	defer logger.Sync()
+
+	// Load the config file based on path provided via CLI or the default
+	configLocation := flag.String("config", "config.json", "path to configuration file")
+	flag.Parse()
+	config := LoadConfiguration(logger, *configLocation)
+
+	// Initialize the InfluxDB connection
+	influxDBAddr := fmt.Sprintf("%s:%d", config.InfluxDBURL, config.InfluxDBPort)
+	host, err := url.Parse(influxDBAddr)
+	if err != nil {
+		logger.Fatal(fmt.Sprintf("failed to parse InfluxDB URL %s", influxDBAddr),
+			zap.String("op", "main"),
+			zap.Error(err),
+		)
+	}
+	influxConf := influxdb.Config{
+		URL: *host,
+	}
+	influxConn, err := influxdb.NewClient(influxConf)
+	if err != nil {
+		logger.Fatal("failed to initialize InfluxDB connection",
+			zap.String("op", "main"),
+			zap.Error(err),
+		)
+	}
+
+	now := time.Now()
+	sunriseTime, sunsetTime := sunrise.SunriseSunset(
+		config.Latitude,
+		config.Longitude,
+		now.Year(),
+		now.Month(),
+		now.Day(),
+	)
+
+	for {
+
+		pollStartTime := int32(time.Now().Unix())
+
+		now = time.Now()
+		sunriseTime, sunsetTime = UpdateSunriseSunset(logger, config, sunriseTime, sunsetTime, now)
+		daylight := Daylight(sunriseTime, sunsetTime, now)
+		err := WriteToInflux(logger, config, influxConn, daylight, now)
+		if err != nil {
+			logger.Error("failed to write daylight data to InfluxDB",
+				zap.String("op", "main"),
+				zap.Error(err),
+			)
+		}
+
+		timeElapsed := int32(time.Now().Unix()) - pollStartTime
+		time.Sleep(config.PollInterval*time.Second - time.Duration(timeElapsed)*time.Second)
+
+	}
+
+}
+
+func UpdateSunriseSunset(logger *zap.Logger, config Configuration, currentSunrise time.Time, currentSunset time.Time, t time.Time) (time.Time, time.Time) {
+	sunriseTime := currentSunrise
+	sunsetTime := currentSunset
+	if currentSunrise.Day() == t.Add(-24*time.Hour).Day() ||
+		currentSunset.Day() == t.Add(-24*time.Hour).Day() {
+
+		logger.Info("fetching new sunrise and sunset values",
+			zap.String("op", "UpdateSunriseSunset"),
+		)
+
+		sunriseTime, sunsetTime = sunrise.SunriseSunset(
+			config.Latitude,
+			config.Longitude,
+			t.Year(),
+			t.Month(),
+			t.Day(),
+		)
+	}
+
+	return sunriseTime, sunsetTime
+
+}
+
+func Daylight(sunrise time.Time, sunset time.Time, t time.Time) bool {
+	if t.Before(sunrise) || t.After(sunset) {
+		return false
+	} else {
+		return true
+	}
+}
+
+func WriteToInflux(logger *zap.Logger, config Configuration, influxConn *influxdb.Client, daylight bool, t time.Time) error {
+	data := make([]influxdb.Point, 1)
+	data[0] = influxdb.Point{
+		Measurement: config.InfluxDBMeasurement,
+		Tags:        map[string]string{},
+		Fields: map[string]interface{}{
+			"daylight": daylight,
+		},
+		Time:      t,
+		Precision: "ns",
+	}
+
+	batchData := influxdb.BatchPoints{
+		Points:          data,
+		Database:        config.InfluxDBDatabase,
+		RetentionPolicy: config.InfluxDBRetentionPolicy,
+	}
+
+	_, err := influxConn.Write(batchData)
+	if err != nil {
+		return err
+	}
+	return nil
+}
